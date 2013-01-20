782f1e845c462ce9c7bcffe2434a439225e2074c
Benjamin Henrion <bh@udev.org>
2013-01-20 21:59:14 +0000

    small script to download tarfiles

---
+++ tarfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TARFILES_DIR="tarfiles"
+TARFILES_URLS="tarfiles.urls"
+
+function banner {
+echo "================================================================"
+echo "$1"
+echo "================================================================"
+}
+
+for url in `cat ${TARFILES_URLS}`; do
+	TGZ="`basename $url`"
+	banner "Downloading $TGZ"
+	wget -c $url -O ${TARFILES_DIR}/`basename $url`
+done
